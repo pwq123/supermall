993d62ae6a608c73b2b2d9bc8e34c5e71da74af6
Peng1997 <8991874+peng1997123@user.noreply.gitee.com>
2021-11-11 22:41:31 +0800

    更新项目

---
+++ src/components/content/checkButton/checkButton.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="check-Button" :class="{check: isChecked}">
+  <img src="../../../../src/assets/img/cart/tick.svg" alt="">
+</div>
+</template>
+
+<script>
+export default {
+  name: "checkButton",
+  props: {
+    isChecked:{
+      type:Boolean,
+      default: false
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.check-Button{
+
+  border-radius: 50%;
+  border: 2px solid #aaa;
+}
+.check {
+  border-color: red;
+  background-color: red;
+}
+</style>
